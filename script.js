31be800cde4eecab20f4d1bcbad00525f3187bc6
SNsaid1998 <61479527+SNsaid1998@users.noreply.github.com>
2021-02-09 19:14:47 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
- require(["esri/Map", "esri/views/MapView"], function (Map, MapView) {
-        var map = new Map({
-          basemap: "dark-gray"
-        });
+var mymap = L.map('mapid').setView([37.75, -122.45], 12);
 
-        var view = new MapView({
-          container: "viewDiv",
-          map: map,
-          zoom: 10,
-          center: [-90, 38.85] // longitude, latitude
-        });
-      });
+L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 20,
+	ext: 'png'
+}).addTo(mymap);
+
+$.getJSON("https://raw.githubusercontent.com/gbrunner/adv-python-for-gis-and-rs/master/Week%201/sf_crime.geojson",function(data){ 
+var ratIcon = L.icon({
+    iconUrl: 'http://maptimeboston.github.io/leaflet-intro/rat.gif',
+    iconSize: [50,40]
+  }); 
+  L.geoJson(data  ,{
+    pointToLayer: function(feature,latlng){
+	  return L.marker(latlng,{icon: ratIcon});
+    }
+  } ).addTo(mymap);
+  });
